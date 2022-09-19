445e9a416f0f14434ba8287305e945af45bbbf61
sudip4141 <95898215+sudip4141@users.noreply.github.com>
2022-09-19 20:57:54 +0530

    sudip

--- client/src/gst filling/Gst_filing.js
+++ client/src/gst filling/Gst_filing.js
@@ -60,16 +60,16 @@ const Gst_filing = () => {
         setLoding(true)
         axios.post('/gst_filling/registration', formdata).then((res) => {
             setLoding(false)
-            if(res.status===201){
+            if (res.status === 201) {
                 alert("successfull")
-            }else{
+            } else {
                 alert("Unsuccessfull !")
             }
         }).catch((e) => {
             setLoding(false)
             alert("Unsuccessfull")
         })
-       
+
         // setyour_name("")
         // setdob("")
         // setmob_number("")
@@ -100,174 +100,174 @@ const Gst_filing = () => {
 
 
     return (<>
-       <div className="gst_fill-main">
-        {
-            loding?(<>
-        <Loader></Loader>
-                </>): (<>
-    <div className='gst_fill'>
-        <div className='head'>
-            <h1>GST FILING Form</h1>
+        <div className="gst_fill-main">
+            {
+                loding ? (<>
+                    <Loader></Loader>
+                </>) : (<>
+                    <div className='gst_fill'>
+                        <div className='head'>
+                            <h1>GST FILING Form</h1>
+                        </div>
+                        <div className='content'>
+                            <div className='inside_content'>
+                                <h1 className='mx-auto w-25 pt-1' style={{ padding: "10px 0" }}>DETAILS</h1>
+                                <hr></hr>
+                                <form onSubmit={handelSumnit}>
+
+                                    <div className='field pt-4'>
+                                        <h5 style={{ color: "black", fontWeight: "bolder" }}>YOUR NAME</h5>
+                                        <input type="text" className='inp' value={your_name} name='your_name' onChange={(e) => {
+                                            setyour_name(e.target.value)
+                                        }}></input>
+                                    </div>
+
+                                    <div className='field pt-4'>
+                                        <h5 style={{ color: "black", fontWeight: "bolder" }}>DATE OF BIRTH</h5>
+                                        <input type="date" className='inp' value={dob} name='dob' onChange={(e) => {
+                                            setdob(e.target.value)
+                                        }}></input>
+                                    </div>
+
+
+                                    <div className='field'>
+                                        <h5 style={{ color: "black", fontWeight: "bolder" }}>Phone Number</h5>
+                                        <input type="number" className='inp' value={mob_number} name='mob_number' onChange={(e) => {
+                                            setmob_number(e.target.value)
+                                        }}></input>
+                                    </div>
+
+                                    <div className='field'>
+                                        <h5 style={{ color: "black", fontWeight: "bolder" }}>Email</h5>
+                                        <input type="email" className='inp' value={email} name='email' onChange={(e) => {
+                                            setemail(e.target.value)
+                                        }}></input>
+                                    </div>
+
+                                    <div className='field pt-4'>
+                                        <h5 style={{ color: "black", fontWeight: "bolder" }}>FATHER'S NAME</h5>
+                                        <input type="text" className='inp' value={father_name} name='father_name' onChange={(e) => {
+                                            setfather_name(e.target.value)
+                                        }}></input>
+                                    </div>
+
+                                    <div className='field'>
+                                        <h5 style={{ color: "black", fontWeight: "bolder" }}>PAN Number</h5>
+                                        <input type="text" className='inp' value={pan_number} name='pan_number' onChange={(e) => {
+                                            setpan_number(e.target.value)
+                                        }}></input>
+                                    </div>
+
+                                    <div className='field'>
+                                        <h5 style={{ color: "black", fontWeight: "bolder" }}>Pan Card Photo</h5>
+                                        <input type="file" className='inp'
+                                            name='pan_photo' onChange={(e) => {
+                                                setpan_photo(e.target.files[0])
+                                            }}></input>
+                                    </div>
+
+                                    <div className='field'>
+                                        <h5 style={{ color: "black", fontWeight: "bolder" }}>BANK STATEMENT FROM 1-4-21 TO 31-2-22</h5>
+                                        <input type="file" className='inp'
+                                            name='bank_statement' onChange={(e) => {
+                                                setbank_statement(e.target.files[0])
+                                            }}></input>
+                                    </div>
+
+                                    <div className='field'>
+                                        <h5 style={{ color: "black", fontWeight: "bolder" }}>LIC PREMIUM IF PAID</h5>
+                                        <input type="file" className='inp'
+                                            name='lic_premium' onChange={(e) => {
+                                                setlic_premium(e.target.files[0])
+                                            }}></input>
+                                    </div>
+
+                                    <div className='field'>
+                                        <h5 style={{ color: "black", fontWeight: "bolder" }}>MEDICAL INSURANCE DETAILS IF PAID</h5>
+                                        <input type="file" className='inp'
+                                            name='medical_insurance' onChange={(e) => {
+                                                setmedical_insurance(e.target.files[0])
+                                            }}></input>
+                                    </div>
+
+                                    <div className='field'>
+                                        <h5 style={{ color: "black", fontWeight: "bolder" }}>AADHAR Number</h5>
+                                        <input type="number" className='inp' value={aadhar_no} name='aadhar_number' onChange={(e) => {
+                                            setaadhar_number(e.target.value)
+                                        }}></input>
+                                    </div>
+
+                                    <div className='field'>
+                                        <h5 style={{ color: "black", fontWeight: "bolder" }}>AADHAR PHOTO</h5>
+                                        <input type="file" className='inp'
+                                            name='aadhar_photo' onChange={(e) => {
+                                                setaadhar_photo(e.target.files[0])
+                                            }}></input>
+                                    </div>
+
+                                    <div className='field'>
+                                        <h5 style={{ color: "black", fontWeight: "bolder" }}>Loan statement stating interest and principal repaid during the last financial year(if had any)</h5>
+                                        <input type="file" className='inp'
+                                            name='loan_statement' onChange={(e) => {
+                                                setloan_statement(e.target.files[0])
+                                            }}></input>
+                                    </div>
+
+
+                                    <div className='field'>
+                                        <h5 style={{ color: "black", fontWeight: "bolder" }}>Donation reciepts if made</h5>
+                                        <input type="file" className='inp'
+                                            name='donation_receipt' onChange={(e) => {
+                                                setdonation_receipt(e.target.files[0])
+                                            }}></input>
+                                    </div>
+
+                                    <div className='field pt-4'>
+                                        <h5 style={{ color: "black", fontWeight: "bolder" }}>Any Properties and Assests registered
+                                            on yor name</h5>
+                                        <input type="text" className='inp' value={property} name='property' onChange={(e) => {
+                                            setproperty(e.target.value)
+                                        }}></input>
+                                    </div>
+
+                                    <div className='field'>
+                                        <h5 style={{ color: "black", fontWeight: "bolder" }}>Complete Current Address</h5>
+                                        <input type="text" className='inp' name='curr_address' value={curr_address} onChange={(e) => {
+                                            setcurr_address(e.target.value)
+                                        }}></input>
+                                    </div>
+
+                                    <div className='field'>
+                                        <h5 style={{ color: "black", fontWeight: "bolder" }}>Complete Permanent Address</h5>
+                                        <input type="text" className='inp' name='permanent_address' value={permanent_address} onChange={(e) => {
+                                            setpermanent_address(e.target.value)
+                                        }}></input>
+                                    </div>
+
+                                    <div className='field'>
+                                        <h5 style={{ color: "black", fontWeight: "bolder" }}>Form 16</h5>
+                                        <input type="file" className='inp'
+                                            name='form_16' onChange={(e) => {
+                                                setform_16(e.target.files[0])
+                                            }}></input>
+                                    </div>
+
+
+                                    <button type='submit'>Submit Form</button>
+                                </form>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </>
+                )
+            }
+
         </div>
-        <div className='content'>
-            <div className='inside_content'>
-                <h1 className='mx-auto w-25 pt-1' style={{ padding: "10px 0" }}>DETAILS</h1>
-                <hr></hr>
-                <form onSubmit={handelSumnit}>
-
-                    <div className='field pt-4'>
-                        <h5 style={{ color: "black", fontWeight: "bolder" }}>YOUR NAME</h5>
-                        <input type="text" className='inp' value={your_name} name='your_name' onChange={(e) => {
-                            setyour_name(e.target.value)
-                        }}></input>
-                    </div>
-
-                    <div className='field pt-4'>
-                        <h5 style={{ color: "black", fontWeight: "bolder" }}>DATE OF BIRTH</h5>
-                        <input type="date" className='inp' value={dob} name='dob' onChange={(e) => {
-                            setdob(e.target.value)
-                        }}></input>
-                    </div>
 
 
-                    <div className='field'>
-                        <h5 style={{ color: "black", fontWeight: "bolder" }}>Phone Number</h5>
-                        <input type="number" className='inp' value={mob_number} name='mob_number' onChange={(e) => {
-                            setmob_number(e.target.value)
-                        }}></input>
-                    </div>
-
-                    <div className='field'>
-                        <h5 style={{ color: "black", fontWeight: "bolder" }}>Email</h5>
-                        <input type="email" className='inp' value={email} name='email' onChange={(e) => {
-                            setemail(e.target.value)
-                        }}></input>
-                    </div>
-
-                    <div className='field pt-4'>
-                        <h5 style={{ color: "black", fontWeight: "bolder" }}>FATHER'S NAME</h5>
-                        <input type="text" className='inp' value={father_name} name='father_name' onChange={(e) => {
-                            setfather_name(e.target.value)
-                        }}></input>
-                    </div>
-
-                    <div className='field'>
-                        <h5 style={{ color: "black", fontWeight: "bolder" }}>PAN Number</h5>
-                        <input type="text" className='inp' value={pan_number} name='pan_number' onChange={(e) => {
-                            setpan_number(e.target.value)
-                        }}></input>
-                    </div>
-
-                    <div className='field'>
-                        <h5 style={{ color: "black", fontWeight: "bolder" }}>Pan Card Photo</h5>
-                        <input type="file" className='inp' 
-                            name='pan_photo' onChange={(e) => {
-                                setpan_photo(e.target.files[0])
-                            }}></input>
-                    </div>
-
-                    <div className='field'>
-                        <h5 style={{ color: "black", fontWeight: "bolder" }}>BANK STATEMENT FROM 1-4-21 TO 31-2-22</h5>
-                        <input type="file" className='inp' 
-                            name='bank_statement' onChange={(e) => {
-                                setbank_statement(e.target.files[0])
-                            }}></input>
-                    </div>
-
-                    <div className='field'>
-                        <h5 style={{ color: "black", fontWeight: "bolder" }}>LIC PREMIUM IF PAID</h5>
-                        <input type="file" className='inp' 
-                            name='lic_premium' onChange={(e) => {
-                                setlic_premium(e.target.files[0])
-                            }}></input>
-                    </div>
-
-                    <div className='field'>
-                        <h5 style={{ color: "black", fontWeight: "bolder" }}>MEDICAL INSURANCE DETAILS IF PAID</h5>
-                        <input type="file" className='inp' 
-                            name='medical_insurance' onChange={(e) => {
-                                setmedical_insurance(e.target.files[0])
-                            }}></input>
-                    </div>
-
-                    <div className='field'>
-                        <h5 style={{ color: "black", fontWeight: "bolder" }}>AADHAR Number</h5>
-                        <input type="number" className='inp' value={aadhar_no}  name='aadhar_number' onChange={(e) => {
-                            setaadhar_number(e.target.value)
-                        }}></input>
-                    </div>
-
-                    <div className='field'>
-                        <h5 style={{ color: "black", fontWeight: "bolder" }}>AADHAR PHOTO</h5>
-                        <input type="file" className='inp' 
-                            name='aadhar_photo' onChange={(e) => {
-                                setaadhar_photo(e.target.files[0])
-                            }}></input>
-                    </div>
-
-                    <div className='field'>
-                        <h5 style={{ color: "black", fontWeight: "bolder" }}>Loan statement stating interest and principal repaid during the last financial year(if had any)</h5>
-                        <input type="file" className='inp' 
-                            name='loan_statement' onChange={(e) => {
-                                setloan_statement(e.target.files[0])
-                            }}></input>
-                    </div>
-
-
-                    <div className='field'>
-                        <h5 style={{ color: "black", fontWeight: "bolder" }}>Donation reciepts if made</h5>
-                        <input type="file" className='inp' 
-                            name='donation_receipt' onChange={(e) => {
-                                setdonation_receipt(e.target.files[0])
-                            }}></input>
-                    </div>
-
-                    <div className='field pt-4'>
-                        <h5 style={{ color: "black", fontWeight: "bolder" }}>Any Properties and Assests registered
-                            on yor name</h5>
-                        <input type="text" className='inp' value={property} name='property' onChange={(e) => {
-                            setproperty(e.target.value)
-                        }}></input>
-                    </div>
-
-                    <div className='field'>
-                        <h5 style={{ color: "black", fontWeight: "bolder" }}>Complete Current Address</h5>
-                        <input type="text" className='inp'  name='curr_address' value={curr_address} onChange={(e) => {
-                            setcurr_address(e.target.value)
-                        }}></input>
-                    </div>
-
-                    <div className='field'>
-                        <h5 style={{ color: "black", fontWeight: "bolder" }}>Complete Permanent Address</h5>
-                        <input type="text" className='inp' name='permanent_address' value={permanent_address} onChange={(e) => {
-                            setpermanent_address(e.target.value)
-                        }}></input>
-                    </div>
-
-                    <div className='field'>
-                        <h5 style={{ color: "black", fontWeight: "bolder" }}>Form 16</h5>
-                        <input type="file" className='inp' 
-                            name='form_16' onChange={(e) => {
-                                setform_16(e.target.files[0])
-                            }}></input>
-                    </div>
-
-
-                    <button type='submit'>Submit Form</button>
-                </form>
-            </div>
-        </div>
-
-    </div>
-  
-</>
-)
- }
-
-            </div>
-      
-
-   </> )
+    </>)
 }
 
 
